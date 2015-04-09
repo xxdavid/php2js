14d016b2f97b68093f35065464871e127f53006b
David Pavlík <david@dpavlik.cz>
2015-04-09 08:54:15 +0200

    Add variable echoing test

---
+++ tests/statement/echo/echoAVariable.js
@@ -0,0 +1,4 @@
+var answer;
+
+answer = 42;
+console.log("The Answer to the Ultimate Question of Life, the Universe, and Everything is " + answer);
---
+++ tests/statement/echo/echoAVariable.php
@@ -0,0 +1,3 @@
+<?php
+$answer = 42;
+echo 'The Answer to the Ultimate Question of Life, the Universe, and Everything is ' . $answer;
